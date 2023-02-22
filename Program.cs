0a06e11c6199c9ef2f8a52e972a05b68771c504a
BrianaSiler334 <120746564+BrianaSiler334@users.noreply.github.com>
2023-02-22 11:25:15 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Transactions;
+
+namespace PayrollArrays {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int employee = int.Parse(Input("Enter employee count:"));
+            string[] employees = new string[employee];
+            
+
+
+            int[] hoursWorked = new int[employee];
+            for (int i = 0; i < hoursWorked.Length; i += 1) {
+                hoursWorked[i] = int.Parse(Input($"Enter hours for employee {i + 1} :"));
+
+
+            }//END FOR
+                Payroll(hoursWorked);
+        }//END MAIN   
+    
+            static void Payroll(int[] array) {
+            double payRate = double.Parse(Input("Enter the hourly rate :$"));
+                    int pay = 0;
+                    for (int index = 0; index < array.Length; index += 1) {
+                      pay = array[index];
+                      Console.WriteLine($"Employee {index + 1} :{pay * payRate:c}");
+                    }//END FOR
+
+            }//END FUNCTION
+         
+            static string Input(string prompt) {
+                    Console.Write(prompt);
+                    return Console.ReadLine();
+            }//END FUNCTION
+
+    }//end class
+}//end namespace
